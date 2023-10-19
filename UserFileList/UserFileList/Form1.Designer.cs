a5138f7a16341ab8fca3aea205b09d5290c402e2
Shigeo Kageyama <89754104+Nuubkid@users.noreply.github.com>
2023-10-19 05:48:55 +0300

    Add files via upload

---
+++ UserFileList/UserFileList/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace UserFileList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.listViewFiles.ForeColor = System.Drawing.Color.Lime;
+            this.listViewFiles.Location = new System.Drawing.Point(2, 3);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(698, 407);
+            this.listViewFiles.TabIndex = 0;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(27, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BROWSE FOLDERS";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(178, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "REMOVE ITEM";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(178, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SAVE USERFILELIST";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Lime;
+            this.button4.Location = new System.Drawing.Point(27, 467);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "BROWSE ITEMS";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(371, 431);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Lime;
+            this.button5.Location = new System.Drawing.Point(454, 459);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "CHECK HASH";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 412);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(708, 13);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(-1, 526);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(85, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "@Nuubkid 2023";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Lime;
+            this.button6.Location = new System.Drawing.Point(103, 503);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 35);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "CLEAR";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(703, 548);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listViewFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UserFileList Generator by CharlieDev34";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewFiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ UserFileList/UserFileList/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace UserFileList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select a folder";
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = folderBrowserDialog.SelectedPath;
+                    List<string> files = GetAllFilesInFolder(folderPath);
+
+                    int totalFiles = files.Count;
+                    int processedFiles = 0;
+
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = totalFiles;
+                    progressBar1.Step = 1;
+
+                    foreach (string file in files)
+                    {
+                        string relativePath = GetRelativePath(folderPath, file);
+                        // Replace forward slashes with backward slashes
+                        relativePath = relativePath.Replace("/", "\\");
+                        string hash = ComputeFileHash(file);
+                        ListViewItem item = new ListViewItem(relativePath);
+                        item.SubItems.Add(hash);
+                        listViewFiles.Items.Add(item);
+
+                        processedFiles++;
+                        progressBar1.Value = processedFiles; // Update the progress bar value
+                    }
+
+                    // Reset the progress bar value to 0
+                    progressBar1.Value = 0;
+                }
+            }
+        }
+
+        private List<string> GetAllFilesInFolder(string folderPath)
+        {
+            List<string> files = new List<string>();
+            string[] subfolders = Directory.GetDirectories(folderPath);
+
+            foreach (string file in Directory.GetFiles(folderPath))
+            {
+                files.Add(file);
+            }
+
+            foreach (string subfolder in subfolders)
+            {
+                files.AddRange(GetAllFilesInFolder(subfolder));
+            }
+
+            return files;
+        }
+
+        private string GetRelativePath(string rootPath, string fullPath)
+        {
+            Uri rootUri = new Uri(rootPath);
+            Uri fullUri = new Uri(fullPath);
+            return Uri.UnescapeDataString(rootUri.MakeRelativeUri(fullUri).ToString());
+        }
+
+        private string ComputeFileHash(string filePath)
+        {
+            using (MD5 md5 = MD5.Create())
+            using (FileStream stream = File.OpenRead(filePath))
+            {
+                byte[] hashBytes = md5.ComputeHash(stream);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in listViewFiles.SelectedItems)
+                {
+                    listViewFiles.Items.Remove(item);
+                }
+            }
+        }
+
+        private void SaveUserFileList(string filePath)
+        {
+            using (XmlTextWriter writer = new XmlTextWriter(filePath, null))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.WriteStartDocument();
+                writer.WriteStartElement("list");
+
+                foreach (ListViewItem item in listViewFiles.Items)
+                {
+                    string fileName = item.Text;
+                    string hash = item.SubItems[1].Text;
+
+                    // Add a backslash to the beginning of the local attribute
+                    string localPath = "\\" + fileName;
+
+                    writer.WriteStartElement("file");
+                    writer.WriteAttributeString("local", localPath);
+                    writer.WriteAttributeString("hash", hash);
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+    
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Title = "Save userFileList.dat";
+                saveFileDialog.Filter = "User File List|userFileList.dat";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    SaveUserFileList(filePath);
+                    MessageBox.Show("userFileList.dat saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select multiple files";
+                openFileDialog.Filter = "All Files|*.*";
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    int totalFiles = openFileDialog.FileNames.Length;
+                    int processedFiles = 0;
+
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = totalFiles;
+                    progressBar1.Step = 1;
+
+                    foreach (string filePath in openFileDialog.FileNames)
+                    {
+                        // Replace forward slashes with backward slashes in the file path
+                        string normalizedFilePath = filePath.Replace("/", "\\");
+
+                        string fileName = Path.GetFileName(normalizedFilePath);
+                        string hash = ComputeFileHash(normalizedFilePath);
+
+                        ListViewItem item = new ListViewItem(fileName);
+                        item.SubItems.Add(hash);
+                        listViewFiles.Items.Add(item);
+
+                        processedFiles++;
+                        progressBar1.Value = processedFiles; // Update the progress bar value
+                    }
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<string> hashes = new List<string>();
+
+            foreach (ListViewItem item in listViewFiles.Items)
+            {
+                string filePath = item.Text;
+                string hash = item.SubItems[1].Text;
+                hashes.Add(hash);
+            }
+
+            // Calculate the hash of all merged files
+            string combinedHash = ComputeCombinedHash(hashes);
+            string hasdhedtext = combinedHash;
+            // Display the combined hash in TextBox1
+            textBox1.Text = hasdhedtext.ToUpper();
+        }
+        private string ComputeCombinedHash(List<string> hashes)
+        {
+            // Concatenate all individual hashes
+            string concatenatedHashes = string.Join("", hashes);
+
+            // Calculate the hash of the concatenated string
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(concatenatedHashes));
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://github.com/Nuubkid";
+
+            // Open the URL in the default web browser
+            System.Diagnostics.Process.Start(url);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            if (listViewFiles.Items.Count > 0)
+            {
+                listViewFiles.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("No List to be clear");
+            }
+        }
+    }
+}
